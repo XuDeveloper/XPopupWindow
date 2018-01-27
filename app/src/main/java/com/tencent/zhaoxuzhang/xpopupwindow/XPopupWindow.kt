c97ef0e3b19bb30a84e33eb8dceb649827519575
XuDeveloper <280745397@qq.com>
2018-01-27 18:20:45 +0800

    [ADD]增加popupwindow的初始化工作

--- app/src/main/java/com/tencent/zhaoxuzhang/xpopupwindow/XPopupWindow.kt
+++ app/src/main/java/com/tencent/zhaoxuzhang/xpopupwindow/XPopupWindow.kt
@@ -1,20 +1,47 @@
 package com.tencent.zhaoxuzhang.xpopupwindow
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup.LayoutParams
 import android.widget.PopupWindow
 
 /**
  * Created by zhaoxuzhang on 2018/1/26.
  */
 
-abstract class XPopupWindow(ctx: Context): PopupWindow(ctx) {
+abstract class XPopupWindow(ctx: Context) {
 
-    private val mPopupView: View?
+    private val TAG = "XPopupWindow"
+
+    //元素定义
+    protected var mPopupWindow: PopupWindow? = null
+
+    protected var mPopupView: View? = null
+
+    constructor(ctx: Context, w: Int, h: Int): this(ctx) {
+        init(ctx, w, h)
+    }
+
+    fun init(ctx: Context, w: Int, h: Int) {
+        mPopupView = LayoutInflater.from(ctx).inflate(getLayoutId(), null)
+        mPopupView!!.isFocusableInTouchMode = true
+
+        mPopupWindow = PopupWindow(ctx)
+        mPopupWindow!!.contentView = mPopupView
+
+        mPopupWindow!!.width = w
+        mPopupWindow!!.height = h
+        mPopupWindow!!.isFocusable = true
+        mPopupWindow!!.isOutsideTouchable = true
+        mPopupWindow!!.setBackgroundDrawable(ColorDrawable())
+
+
+    }
 
     init {
-        mPopupView = LayoutInflater.from(ctx).inflate(getLayoutId(), null)
+        init(ctx, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         initViews()
 
     }
